8ed6d1ce7b758855e20747313f30890096ed72c5
Michael Binshtock <michabin@microsoft.com>
2018-09-23 02:53:23 +0300

    Fixed timespan

--- src/kql/la_client.py
+++ src/kql/la_client.py
@@ -14,7 +14,7 @@ import requests
 import webbrowser
 
 # Regex for TimeSpan
-TIMESPAN_PATTERN = re.compile(r"((?P<d>[0-9]*).)?(?P<h>[0-9]{2}):(?P<m>[0-9]{2}):(?P<s>[0-9]{2})(.(?P<ms>[0-9]*))?")
+_TIMESPAN_PATTERN = re.compile(r"(-?)((?P<d>[0-9]*).)?(?P<h>[0-9]{2}):(?P<m>[0-9]{2}):(?P<s>[0-9]{2}(\.[0-9]+)?$)")
 
 __version__ = "0.1.0"
 
@@ -82,22 +82,22 @@ class LoganalyticsResponseTable(six.Iterator):
 
     @staticmethod
     def to_timedelta(value):
+        """Converts a string to a timedelta."""
         if value is None:
             return None
-        if isinstance(value, (six.integer_types, float)): 
-            return timedelta(microseconds=(float(value) / 10))         
-        m = TIMESPAN_PATTERN.match(value)
-        if m:
-            if match.group(1) == "-": 
-                factor = -1 
-            else: 
-                factor = 1 
+        if isinstance(value, (six.integer_types, float)):
+            return timedelta(microseconds=(float(value) / 10))
+        match = _TIMESPAN_PATTERN.match(value)
+        if match:
+            if match.group(1) == "-":
+                factor = -1
+            else:
+                factor = 1
             return factor * timedelta(
-                days=int(m.group("d") or 0),
-                hours=int(m.group("h")),
-                minutes=int(m.group("m")),
-                seconds=int(m.group("s")),
-                milliseconds=int(m.group("ms") or 0),
+                days=int(match.group("d") or 0),
+                hours=int(match.group("h")),
+                minutes=int(match.group("m")),
+                seconds=float(match.group("s")),
             )
         else:
             raise ValueError("Timespan value '{}' cannot be decoded".format(value))

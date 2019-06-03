c95e3e994f60ccd5dec6b72aab2c2ce7f23eefd3
Avital Friedland <t-avfrie@microsoft.com>
2019-06-03 16:20:25 +0300

    changing nose to pytest framework

--- azure/tests/test_basics.py
+++ azure/tests/test_basics.py
@@ -4,8 +4,7 @@
 # license information.
 #--------------------------------------------------------------------------
 
-from nose import with_setup
-from nose.tools import raises
+import pytest
 from Kqlmagic.constants import Constants
 from Kqlmagic.kql_magic import Kqlmagic as Magic
 from textwrap import dedent
@@ -15,46 +14,42 @@ import tempfile
 
 ip = get_ipython() # pylint: disable=E0602
 
-def setup():
+
+@pytest.fixture 
+def register_magic():
     magic = Magic(shell=ip)
     ip.register_magics(magic)
 
-def _setup():
-    pass
-
-def _teardown():
-    pass
-
 TEST_URI_SCHEMA_NAME = "kusto"
-
 query1 = "-conn=$TEST_CONNECTION_STR let T = view () { datatable(n:long, name:string)[1,'foo',2,'bar'] }; T"
 
 version_command = "--version"
 version_pw_command = version_command + " -pw"
 version_expected_pattern = r'Kqlmagic version: [0-9]+\.[0-9]+\.[0-9]+'
 
-@with_setup(_setup, _teardown)
-def test_ok():
+def test_ok(register_magic):
     assert True
 
-@with_setup(_setup, _teardown)
-def test_version():
+
+#Testing "--version" command
+def test_version(register_magic):
     result = ip.run_line_magic('kql', version_command)
     version_str = str(result)
     print(version_str)
     expected_pattern = r'^' + version_expected_pattern  + r'$'
     assert re.search(expected_pattern , version_str)
 
-@with_setup(_setup, _teardown)
-def test_version_pw_button():
+
+#Testing "--version" command with pop up window button
+def test_version_pw_button(register_magic):
     result = ip.run_line_magic('kql', version_pw_command)
     pw_html_str = result._repr_html_()
     print(pw_html_str)
     assert re.search(r'<button', pw_html_str)
     assert re.search(r'>popup version </button>', pw_html_str)
 
-@with_setup(_setup, _teardown)
-def test_version_pw_file():
+#Testing "--version" command with pop up window button
+def test_version_pw_file(register_magic):
     result = ip.run_line_magic('kql', version_pw_command)
     pw_html_str = result._repr_html_()
     print(pw_html_str)
@@ -69,12 +64,17 @@ def test_version_pw_file():
     expected_pattern = r'^<p>' + version_expected_pattern  + r'</p>$'
     assert re.search(expected_pattern , version_html_str)
 
-# def test_fail():
-#     assert False    
 
-@with_setup(_setup, _teardown)
-def test_query():
+#This method creates a table with query 1 and checks its components. The table created is:
+# +---+------+
+# | n | name |
+# +---+------+
+# | 1 | foo  |
+# | 2 | bar  |
+
+def test_query(register_magic):
     result = ip.run_line_magic('kql', query1)
     print(result)
     assert result[0][0] == 1
     assert result[1]['name'] == 'bar'
+

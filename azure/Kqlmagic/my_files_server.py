7976ed25562abde084f44fa97704ebcf3a9b6a04
Michael Binshtock <michabin@microsoft.com>
2020-03-09 10:59:38 +0200

    new files server for temp files

---
+++ azure/Kqlmagic/my_files_server.py
@@ -0,0 +1,100 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+import sys
+import os
+from datetime import datetime
+
+
+from flask import Flask, send_file, request, make_response
+
+
+DEFAULT_PORT = "5000"
+DEFAULT_HOST = "127.0.0.1"
+
+
+base_folder = None
+folderlist = []
+
+app = Flask("kqlmagic_temp_files_server")
+
+
+@app.after_request
+def after_request_func(response):
+    """disable server and client cache"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route('/files/<foldername>/<filename>')
+def files(foldername, filename):
+    """return content of filename as the response body."""
+    if len(folderlist) == 0 or foldername in folderlist:
+        err_resp = check_path(foldername, filename)
+        if err_resp is not None:
+            return err_resp
+        file_path = f"{base_folder}/{foldername}/{filename}"
+        print(f"files file_path: {file_path}")
+        return send_file(file_path)
+
+
+@app.route('/folders/<foldername>')
+def folders(foldername):
+    """return content of filename as the response body."""
+    if len(folderlist) == 0 or foldername in folderlist:
+        filename = request.args.get("filename")
+        err_resp = check_path(foldername, filename)
+        if err_resp is not None:
+            return err_resp
+        file_path = f"{base_folder}/{foldername}/{filename}"
+        print(f"folders file_path: {file_path}")
+        return send_file(file_path)
+
+
+@app.route('/ping')
+def ping():
+    """print 'pong' as the response body."""
+    return 'pong'
+
+
+@app.route('/abort')
+def abort():
+    """aborts the process"""
+    os.abort()
+    return ''
+
+
+def check_path(foldername, filename):
+    err_resp = None
+    if not os.path.exists(f"{base_folder}"):
+        err_resp = make_response("Base folder not found", 404)
+    elif not os.path.exists(f"{base_folder}/{foldername}"):
+        err_resp = make_response("Folder not found", 404)
+    elif not os.path.exists(f"{base_folder}/{foldername}/{filename}"):
+        err_resp = make_response("File not found", 404)
+    return err_resp
+
+
+if __name__ == "__main__":
+    params = {}
+    key = None
+    for arg in sys.argv[1:]:
+        if key is not None:
+            params[key] = arg
+            key = None
+        elif arg.startswith('-'):
+            key = arg[1:]
+
+    base_folder = params.get('base_folder')       
+    if base_folder is not None:
+        base_folder = base_folder[:-1] if base_folder.endswith('/') else base_folder
+        folders = params.get('folders', [])
+        folderList = [f[:-1] if f.endswith('/') else f for f in folders.split(",")]
+        port = params.get('port', DEFAULT_PORT)
+        host = params.get('host', DEFAULT_HOST)
+        app.run(host=host, port=port)
---
+++ azure/Kqlmagic/my_files_server_management.py
@@ -0,0 +1,67 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+
+import sys
+import os
+import subprocess as sub
+import atexit
+
+
+import requests
+
+
+class FilesServerManagement(object):
+
+
+    def __init__(self, server_py_code, protocol, host, port, base_folder, folders, **kwargs):
+        self._server_py_code = server_py_code
+        self._protocol = protocol
+        self._host = host
+        self._port = port
+        self._base_folder = base_folder
+        self._folders = folders 
+        self._server_url = f"{self._protocol}://{self._host}:{self._port}"
+
+
+    @property
+    def files_url(self) -> str:
+        return f"{self._server_url}/files"
+
+
+    @property
+    def folders_url(self) -> str:
+        return f"{self._server_url}/folders"
+
+
+    def startServer(self):
+        if not self.pingServer():
+            command = f"start /min /wait  python {self._server_py_code} -protocol {self._protocol} -host {self._host} -port {self._port} -base_folder {self._base_folder} -folders {self._folders}"
+            sub.Popen(command, shell=True)
+
+        atexit.register(FilesServerManagement._abortServer, self._server_url)
+
+
+    def pingServer(self):
+        try:
+            ping_url = f"{self._server_url}/ping"
+            requests.get(ping_url)
+            return True
+        except:
+            return False
+
+
+    def abortServer(self):
+        FilesServerManagement._abortServer(self._server_url)
+
+
+    @staticmethod
+    def _abortServer(server_url):
+        try:
+            abort_url = f"{server_url}/abort"
+            requests.get(abort_url)
+        except:
+            None

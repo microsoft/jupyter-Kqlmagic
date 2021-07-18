a0e3a771631cdeff8c9482c943fa5101d32723f1
Michael Binshtock <michabin@microsoft.com>
2021-07-18 18:45:38 +0300

    sign version

--- azure/Kqlmagic/_version.py
+++ azure/Kqlmagic/_version.py
@@ -28,7 +28,7 @@
 # (1, 2, 0, 'final', 0) => "1.2"
 
 
-__version_info__ = (0, 1, 114, "dev", 27)
+__version_info__ = (0, 1, 114, "final", 0)
 
 assert len(__version_info__) == 5
 assert __version_info__[3] in ('dev', 'alpha', 'beta', 'rc', 'final', 'post')

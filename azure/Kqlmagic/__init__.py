04eecac05bbabafa6e27c08653304df9f6b0951d
Michael Binshtock <michabin@microsoft.com>
2022-01-08 03:29:13 +0200

    fixed not wheel install

--- azure/Kqlmagic/__init__.py
+++ azure/Kqlmagic/__init__.py
@@ -12,7 +12,7 @@ except:
     pass
 
 from .magic_extension import load_ipython_extension, unload_ipython_extension
-from .version import __version__
+from ._version import __version__
 from .kql_magic import kql, kql_stop
 
 __all__ = ['__version__', 'kql', 'kql_stop', 'load_ipython_extension', 'unload_ipython_extension']
